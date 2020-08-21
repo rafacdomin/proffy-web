5b1d3a5f6b8633995de031d17999f03c38a848b3
Rafael <rafaelcodomingues@gmail.com>
2020-08-21 17:17:12 -0300

    :rocket: Fix to deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //https://rafaelcodomingues-proffy.herokuapp.com/
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://rafaelcodomingues-proffy.herokuapp.com',
 });
 
 export default api;
